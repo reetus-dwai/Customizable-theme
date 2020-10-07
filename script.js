862a501d315f95ada575f899efe548a0242849cc
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-10-07 14:33:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function ready() {
-    alert('DOM is ready');
+    if (localStorage.length() > 0){
+      for (i = 0; i < (document.getElementsByClassName('theme').length); i++){
+        document.getElementsByClassName('theme')[i].style.background = localStorage.getItem('background');
+        document.getElementsByClassName('theme')[i].style.color = localStorage.getItem('color');
+    }
+    } else {
+      alert('You have 0 saved styles.');    
+    }
   }
-
-  document.addEventListener("DOMContentLoaded", ready);
-
-// function start(){
-//   for (i = 0; i < (document.getElementsByClassName('theme').length); i++){
-//     document.getElementsByClassName('theme')[i].style.background = localStorage.getItem('background');
-//     document.getElementsByClassName('theme')[i].style.color = localStorage.getItem('color');
-//   }
-// }
+document.addEventListener("DOMContentLoaded", ready);
 
 function flip(){
   if (document.getElementById('settings').style.display == 'none'){
     document.getElementById('settings').style.display = 'block';
   } else {
     document.getElementById('settings').style.display = 'none';
-}
+  }
 }
             
 function theme(){
@@ -31,6 +30,7 @@ function save(){
     document.getElementsByClassName('theme')[i].style.background = document.getElementById('background').value;
     document.getElementsByClassName('theme')[i].style.color = document.getElementById('color').value;
   }
+  localStorage.clear();
   localStorage.setItem('background', document.getElemtentById('background').value);
   localStorage.setItem('color', document.getElemtentById('color').value);
 }
