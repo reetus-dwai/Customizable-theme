2b3a51d77402a25ef6e4b25e1d96acb5f2340315
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-10-06 19:13:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function flip(){
   if (document.getElementById('settings').style.display = 'none'){
-    document.getElementById('settings').style.display = 'block';    
+    document.getElementById('settings').style.display = 'block';
+    alert(document.getElementByI('settings').style.display);
   } else {
     document.getElementById('settings').style.display = 'none';
+    alert(document.getElementByI('settings').style.display);
   }
 }
                
