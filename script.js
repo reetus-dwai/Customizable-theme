c558aebcc93aab3903f3e297ee222404b0763c13
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-10-06 19:18:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ function theme(){
     document.getElementsByClassName('theme')[i].style.color = document.getElementById('color').value;
   }
 }
+
+function save(){
+  localStorage.setItem('background', document.getElemtentById('background').value)
+  localStorage.setItem('background', document.getElemtentById('color').value)
+}
